42d6295621543bfc6ae64c7daf84a293ddc7ddf4
wolfecd09 <wolfecd09@gmail.com>
2017-02-04 00:13:43 -0600

    Update playlists.js

--- tplayer/js/playlists.js
+++ tplayer/js/playlists.js
@@ -2,7 +2,7 @@ var playlist_01 = [{
 	"type": "audio",
 	"artist": "Wolf Measure",
 	"title": "The Sun Rises",
-	"audio": "https://raw.githubusercontent.com/wolfecd09/wolfmeasure/Resources/Audio/The Sun Rises.mp3",
+	"audio": "https://raw.githubusercontent.com/wolfecd09/wolfmeasure.master/Resources/Audio/The Sun Rises.mp3",
 	"cover": "http://www.id-fis.ru/demo/wp/wp-content/uploads/Wild-Beasts–Get-My-Bang.jpg"
 }, {
 	"type": "audio",
